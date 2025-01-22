b0aacf27e3d58f166721e02330ae88e734cfec84
Maria Gabriela Martínez <gabrielamg@Marias-MacBook-Air.local>
2025-01-22 17:48:47 -0300

    addition of the getTaskList route. It returns all tasks from one user

--- routes/tasks.js
+++ routes/tasks.js
@@ -4,7 +4,8 @@ const validateRequest = require('../middlewares/auth')
 
 const router = express.Router();
 router.post('/createTask', validateRequest, taskController.createTask); // 
-router.get('/getTask', taskController.getTask);// handles GET requests
+router.get('/getTask', validateRequest, taskController.getTask);// handles GET requests
+router.get('/getTaskList', validateRequest, taskController.getTaskList);// handles GET requests
 router.post('/modifyTask', taskController.modifyTask); // 
 
 
