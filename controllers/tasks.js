63e3a1f9864a74265707ca440fb4ffaf568c4665
Maria Gabriela Martínez <gabrielamg@Marias-MacBook-Air.local>
2025-01-22 17:48:08 -0300

    addition of getTaskList controller

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -6,7 +6,7 @@ module.exports = {
     createTask: async (req, res) => {
         try {
             const { title, description, dueDate } = req.body;
-            const userId = parseInt(req.user); // Get user ID from the req since it was populated by the middleware
+            const userId = req.user; // Get user ID from the req since it was populated by the middleware
             const newTask = await db.Task.create({
                 title: title,
                 description: description,
@@ -19,8 +19,30 @@ module.exports = {
             res.status(500).json({ error: 'Failed to create task' });
         }
     },
+    getTaskList: async (req, res) => {
+        try {
+            const userId = req.user
+            console.log('this is the id received', userId)
+            const userTasks = await db.Task.findAll({
+                where: { UserId: userId }
+            })
+            res.status(200).json({ message: 'Here is your list of tasks', list: userTasks});
+        } catch (error) {
+            console.error('Error creating task:', error);
+            res.status(500).json({ error: 'Failed to create task' });
+        }
+    },
     getTask: async (req, res) => {
-
+        try {
+            const userId = req.user
+            const userTasks = db.Task.findAll({
+                where: { UserId: userId }
+            })
+            res.status(201).json({ message: 'Task created successfully', task: newTask });
+        } catch (error) {
+            console.error('Error creating task:', error);
+            res.status(500).json({ error: 'Failed to create task' });
+        }
     },
     modifyTask: (req, res) => {
         res.send('hello modify')
