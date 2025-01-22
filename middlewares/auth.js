f5bfa55fe779a2ae0c06377a26a2dc6d10217b3a
Maria Gabriela Martínez <gabrielamg@Marias-MacBook-Air.local>
2025-01-22 16:14:24 -0300

    middleware for auth added

---
+++ middlewares/auth.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+
+const validateRequest = (req, res, next) => {
+    const token = req.headers.authorization?.split(' ')[1]; 
+    if (!token) {
+      return res.status(401).json({ message: 'Unauthorized' });
+    }
+  
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+      if (err) {
+        return res.status(401).json({ message: 'Invalid token' });
+      }
+      req.user = decoded.id; // it populates with the id
+      console.log('this is req', decoded)
+      next(); 
+    });
+  };
+module.exports = validateRequest;
