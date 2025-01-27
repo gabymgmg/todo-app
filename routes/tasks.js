9f4dd4f4d1962d80e665511a6dcde066518b8485
Maria Gabriela Martínez <gabrielamg@Marias-MacBook-Air.local>
2025-01-27 14:24:16 -0300

    route for modify and delete by id added

--- routes/tasks.js
+++ routes/tasks.js
@@ -6,7 +6,7 @@ const router = express.Router();
 router.post('/tasks', validateRequest, taskController.createTask); // Create Task
 router.get('/tasks', validateRequest, taskController.getAllTasks);// Return all tasks by userId
 router.get('/tasks/:taskId', validateRequest, taskController.getTaskById);// Return one task
-router.put('/task/:taskId', validateRequest, taskController.modifyTask); // Modify a task
-router.delete('/tasks:taskId',validateRequest, taskController.deleteTask); // 
+router.put('/tasks/:taskId', validateRequest, taskController.modifyTask); // Modify a task
+router.delete('/tasks/:taskId',validateRequest, taskController.deleteTaskById); // 
 
 module.exports = router;    
