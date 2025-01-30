6d23976566497334bae261ce917fcbf131c08c9e
Maria Gabriela Martínez <gabrielamg@Marias-MacBook-Air.local>
2025-01-30 17:41:38 -0300

    name od the middleware changed

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const taskController = require('../controllers/tasks');
-const validateRequest = require('../middlewares/auth')
+const validateAccessToken = require('../middlewares/auth')
 
 const router = express.Router();
-router.post('/tasks', validateRequest, taskController.createTask); // Create Task
-router.get('/tasks', validateRequest, taskController.getAllTasks);// Return all tasks by userId
-router.get('/tasks/:taskId', validateRequest, taskController.getTaskById);// Return one task
-router.put('/tasks/:taskId', validateRequest, taskController.modifyTask); // Modify a task
-router.delete('/tasks/:taskId',validateRequest, taskController.deleteTaskById); // 
+router.post('/tasks', validateAccessToken, taskController.createTask); // Create Task
+router.get('/tasks', validateAccessToken, taskController.getAllTasks);// Return all tasks by userId
+router.get('/tasks/:taskId', validateAccessToken, taskController.getTaskById);// Return one task
+router.put('/tasks/:taskId', validateAccessToken, taskController.modifyTask); // Modify a task
+router.delete('/tasks/:taskId',validateAccessToken, taskController.deleteTaskById); // 
 
 module.exports = router;    
