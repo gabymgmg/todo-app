11742e1a729777502c948ab1bc0198197cba7915
Maria Gabriela Martínez <gabrielamg@Marias-MacBook-Air.local>
2025-02-03 17:22:57 -0300

    sending the user name to client in order to show it

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -1,6 +1,5 @@
 module.exports = {
     dashboardView: (req, res) => {
-      res.render('dashboard', { user: req.user }); 
-      console.log('this is', req.user)
+      res.render('dashboard', { name: req.user.name }); 
     }
   }
