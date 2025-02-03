69d00465ace0f3b6a2e8bbb0343f23e2510eb8c4
Maria Gabriela Martínez <gabrielamg@Marias-MacBook-Air.local>
2025-02-03 17:25:38 -0300

    changes to dashboard to show username

--- public/script.js
+++ public/script.js
@@ -1,62 +1,65 @@
-async function handleLoginResponse(response) {
-  if (response.ok) {
-    try {
-      const data = await response.json();
-      const token = data.accessToken
-      // Get the access token
-      if (token) {
-        // Save the access token (short-lived) in browser session
-        sessionStorage.setItem('jwtToken', token);
-        console.log('this is access', token)
-        //Fetch dashboard
-        const dashboardResponse = await fetch('/dashboard', {
-          method: 'GET',
-          headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${token}`
-          }
+// async function handleLoginResponse(response) {
+//   if (response.ok) {
+//     try {
+//       const data = await response.json();
+//       console.log('this is access', data.accessToken)
 
-        });
-        console.log(dashboardResponse.ok)
-        if (dashboardResponse.ok) {
-          // Redirect only if the dashboard fetch is successful.
-        window.location.href = '/dashboard'; // Or redirect to the actual dashboard URL.
-        }
-      } else {
-        console.error('No access token received.');
-        alert('Login failed. No access token received.');
-      }
-    } catch (error) {
-      console.error('Error parsing JSON:', error);
-      alert('Login failed. An error occurred.');
-    }
-  } else {
-    // Handle login errors
-    console.error('Login failed:', response.status);
-    alert('Login failed. Please check your credentials.');
-  }
-}
+//       const token = data.accessToken
+//       // Get the access token
+//       if (token) {
+//         // Save the access token (short-lived) in browser session
+//         sessionStorage.setItem('jwtToken', token);
+//         console.log('this is access', token)
+//         // Fetch dashboard
+//         // const dashboardResponse = await fetch('/dashboard', {
+//         //   method: 'GET',
+//         //   headers: {
+//         //     'Content-Type': 'application/json',
+//         //     'Authorization': `Bearer ${token}`
+//         //   }
 
-const loginForm = document.getElementById('loginForm');
+//         // });
+//         // console.log(dashboardResponse.ok)
+//         // if (dashboardResponse.ok) {
+//         //   // Redirect only if the dashboard fetch is successful.
+//         window.location.href = '/dashboard'; // Or redirect to the actual dashboard URL.
+//         // }
+//       } else {
+//         console.error('No access token received.');
+//         alert('Login failed. No access token received.');
+//       }
+//     } catch (error) {
+//       console.error('Error parsing JSON:', error);
+//       alert('Login failed. An error occurred.');
+//     }
+//   } else {
+//     // Handle login errors
+//     console.error('Login failed:', response.status);
+//     alert('Login failed. Please check your credentials.');
+//   }
+// }
 
-loginForm.addEventListener('submit', async (event) => {
-  console.log('submitted')
-  event.preventDefault();
+// const loginForm = document.getElementById('loginForm');
 
-  const formData = new FormData(loginForm);
-  const data = Object.fromEntries(formData);
+// loginForm.addEventListener('submit', async (event) => {
+//   console.log('submitted')
+//   event.preventDefault();
 
-  try {
-    const response = await fetch('/login', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(data)
-    });
+//   const formData = new FormData(loginForm);
+//   const data = Object.fromEntries(formData);
 
-    handleLoginResponse(response);
+//   try {
+//     const response = await fetch('/login', {
+//       method: 'POST',
+//       headers: { 'Content-Type': 'application/json' },
+//       body: JSON.stringify(data)
+//     });
+//     console.log(response)
 
-  } catch (error) {
-    console.error('Login error:', error);
-    alert('An error occurred during login.');
-  }
-});
+//     handleLoginResponse(response);
+
+//   } catch (error) {
+//     console.error('Login error:', error);
+//     alert('An error occurred during login.');
+//   }
+// });
