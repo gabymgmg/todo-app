d727934f284855c1fb235bc12cae1b96c2646d94
Maria Gabriela Martínez <gabrielamg@Marias-MacBook-Air.local>
2025-02-03 17:27:31 -0300

    dashboard view updated to show name. Dashboard route middleware updated to use passport

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const dashboardController = require('../controllers/dashboard');
-const validateRequest = require('../middlewares/auth')
-
+const validateAccessToken = require('../middlewares/auth')
+const passport = require('../passport')
 const router = express.Router();
-router.get('/dashboard', dashboardController.dashboardView);
+const { protectRoute } = require('../passport');
+
+router.get('/dashboard',protectRoute, dashboardController.dashboardView);
 
 module.exports = router;
