0c88d41f0dc31c5151bba04c509c9a3ca43c644b
Maria Gabriela Martínez <gabrielamg@Marias-MacBook-Air.local>
2025-02-06 18:30:30 -0300

    options object changed for extract from cookies

--- passport.js
+++ passport.js
@@ -4,38 +4,11 @@ const ExtractJwt = require('passport-jwt').ExtractJwt;
 const db = require('./models/index');
 const LocalStrategy = require('passport-local');
 const bcrypt = require('bcryptjs');
+
 const options = {
-    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    jwtFromRequest: ExtractJwt.fromExtractors([(req) => req.cookies.jwt]), // Extract it from the cookies
     secretOrKey: process.env.JWT_SECRET,
 };
-// module.exports = {
-//     init: () =>
-//         passport.use(
-//             new JwtStrategy(options, (jwtPayload, done) => {
-//                 // Use the decoded user ID (jwtPaysload.id) to fetch user data from your database
-//                 db.User.findOne({ where: { id: jwtPayload.id } })
-//                     .then(user => {
-//                         if (user) {
-//                             return done(null, user); // Successful verification
-//                         } else {
-//                             return done(null, false); // User not found
-//                         }
-//                     })
-//                     .catch(error => {
-//                         console.error('Error finding user by ID:', error);
-//                         return done(error); // Internal server error
-//                     });
-//             })
-//         )
-//         passport.serializeUser((user, done) => {;
-//             done(null, user.id);
-//           });
-
-
-
-// }
-
-
 module.exports = {
     init: () => {
         passport.use('local', new LocalStrategy({ usernameField: 'email' }, async (email, password, done) => {
@@ -54,7 +27,7 @@ module.exports = {
             }
         }));
         passport.use('jwt', new JwtStrategy(options, (jwt_payload, done) => {
-            User.findById(jwt_payload.id)
+            db.User.findByPk(jwt_payload.id)
                 .then(user => {
                     if (user) {
                         return done(null, user);
@@ -64,7 +37,6 @@ module.exports = {
                 .catch(err => done(err, null));
         }));
         passport.serializeUser((user, done) => {
-            ;
             done(null, user.id);
         });
 
@@ -78,7 +50,7 @@ module.exports = {
         if (req.isAuthenticated()) {
             return next();
         }
-        res.redirect('/login?next=' + req.url);
+        res.redirect('/login');
     }
 };
 
