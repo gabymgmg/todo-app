269a5e794c7466420cc35fd8bc31354382530fd1
Maria Gabriela Martínez <gabrielamg@Marias-MacBook-Air.local>
2025-02-06 18:28:44 -0300

    event listener added to submit. Redirection to dashboard added -now with cookies-

--- public/script.js
+++ public/script.js
@@ -1,65 +1,27 @@
-// async function handleLoginResponse(response) {
-//   if (response.ok) {
-//     try {
-//       const data = await response.json();
-//       console.log('this is access', data.accessToken)
+const loginForm = document.getElementById('loginForm');
 
-//       const token = data.accessToken
-//       // Get the access token
-//       if (token) {
-//         // Save the access token (short-lived) in browser session
-//         sessionStorage.setItem('jwtToken', token);
-//         console.log('this is access', token)
-//         // Fetch dashboard
-//         // const dashboardResponse = await fetch('/dashboard', {
-//         //   method: 'GET',
-//         //   headers: {
-//         //     'Content-Type': 'application/json',
-//         //     'Authorization': `Bearer ${token}`
-//         //   }
+loginForm.addEventListener('submit', async (event) => {
+    event.preventDefault();
 
-//         // });
-//         // console.log(dashboardResponse.ok)
-//         // if (dashboardResponse.ok) {
-//         //   // Redirect only if the dashboard fetch is successful.
-//         window.location.href = '/dashboard'; // Or redirect to the actual dashboard URL.
-//         // }
-//       } else {
-//         console.error('No access token received.');
-//         alert('Login failed. No access token received.');
-//       }
-//     } catch (error) {
-//       console.error('Error parsing JSON:', error);
-//       alert('Login failed. An error occurred.');
-//     }
-//   } else {
-//     // Handle login errors
-//     console.error('Login failed:', response.status);
-//     alert('Login failed. Please check your credentials.');
-//   }
-// }
-
-// const loginForm = document.getElementById('loginForm');
-
-// loginForm.addEventListener('submit', async (event) => {
-//   console.log('submitted')
-//   event.preventDefault();
-
-//   const formData = new FormData(loginForm);
-//   const data = Object.fromEntries(formData);
-
-//   try {
-//     const response = await fetch('/login', {
-//       method: 'POST',
-//       headers: { 'Content-Type': 'application/json' },
-//       body: JSON.stringify(data)
-//     });
-//     console.log(response)
-
-//     handleLoginResponse(response);
-
-//   } catch (error) {
-//     console.error('Login error:', error);
-//     alert('An error occurred during login.');
-//   }
-// });
+    const formData = new FormData(loginForm);
+    const data = Object.fromEntries(formData);
+    console.log(data)
+    try {
+        const response = await fetch('/login', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(data)
+        });
+        if (response.ok) {
+            // Redirect to dashboard immediately after successful login
+            window.location.href = '/dashboard'; 
+          } else {
+            // Handle login errors
+            console.error('Login failed:', response.status);
+            alert('Login failed. Please check your credentials.');
+          }
+    } catch (error) {
+        console.error('Login error:', error); // Handle errors during the fetch request itself, such as network errors, connection issues,etc.
+        alert('An error occurred during login.');
+    }
+});
