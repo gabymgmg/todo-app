392dab0f2c4614ab65e8628e917ab2f3fd50fbda
Maria Gabriela Martínez <gabrielamg@Marias-MacBook-Air.local>
2025-02-06 18:36:31 -0300

    Passport auth added to dashboard -it now authenticates with jwt

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const dashboardController = require('../controllers/dashboard');
-const validateAccessToken = require('../middlewares/auth')
-const passport = require('../passport')
+//const validateAccessToken = require('../middlewares/auth')
+const passport = require('passport')
 const router = express.Router();
-const { protectRoute } = require('../passport');
 
-router.get('/dashboard',protectRoute, dashboardController.dashboardView);
+router.get('/dashboard',passport.authenticate('jwt', { session: false }), dashboardController.dashboardView);
 
 module.exports = router;
