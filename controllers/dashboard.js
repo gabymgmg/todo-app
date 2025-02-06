1251b204a7787825a98b2ad86d1e7c2d0cbab2dd
Maria Gabriela Martínez <gabrielamg@Marias-MacBook-Air.local>
2025-02-06 18:35:25 -0300

    dahsboard controller modified. Now it checkks if req.user exists

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -1,5 +1,16 @@
+const bcrypt = require('bcryptjs');
+const db = require('../models/index');
+const jwt = require('jsonwebtoken');
+const config = require('../config/config.js')
+const configDev = config.development
+const passport = require('passport')
+
 module.exports = {
-    dashboardView: (req, res) => {
+  dashboardView: (req, res) => {
+    if (req.user) { 
       res.render('dashboard', { name: req.user.name }); 
+    } else {
+      res.render('login')
     }
   }
+}
