37f716f9b3ad29e375d3c05d2ab0696ed43a33dd
Maria Gabriela Martínez <gabrielamg@Marias-MacBook-Air.local>
2025-02-10 12:51:47 -0300

    dashboard shows the list of tasks

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -6,9 +6,18 @@ const configDev = config.development
 const passport = require('passport')
 
 module.exports = {
-  dashboardView: (req, res) => {
-    if (req.user) { 
-      res.render('dashboard', { name: req.user.name }); 
+  dashboardView: async (req, res) => {
+    if (req.user) {
+      try {
+        const tasks = await db.Task.findAll({ 
+          where: { UserId: req.user.id } 
+        }); 
+        res.render('dashboard', { name: req.user.name ,tasks: tasks });
+      }
+      catch (err) {
+        console.error('Error fetching tasks:', err);
+        res.status(500).send('Error fetching tasks.');
+      }
     }
   }
 }
