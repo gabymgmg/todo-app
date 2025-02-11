ad9a33979edc6672b9a5247f5ceeaa1ca6a8581f
Maria Gabriela Martínez <gabrielamg@Marias-MacBook-Air.local>
2025-02-11 18:11:22 -0300

    delete task function added

--- public/tasks.js
+++ public/tasks.js
@@ -1,26 +1,49 @@
 const myModal = new bootstrap.Modal(document.getElementById('addTaskModal'))
 const addTaskForm = document.getElementById('addTaskForm');
+
 addTaskForm.addEventListener('submit', async (event) => {
-  event.preventDefault();
+    event.preventDefault();
 
-  const formData = new FormData(addTaskForm);
-  const data = Object.fromEntries(Array.from(formData.entries())); 
+    const formData = new FormData(addTaskForm);
+    const data = Object.fromEntries(Array.from(formData.entries()));
+    try {
+        const response = await fetch('/tasks', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(data)
+        });
 
-  try {
-    console.log('This is data sent', data); // Log to verify event listener execution
-    const response = await fetch('/tasks', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(data)
-    });
+        if (response.ok) {
+            myModal.hide();
+            location.reload();
+        } else {
+            // Handle task creation errors
+            const errorData = await response.json();
+            const errorMessage = errorData.message || 'Empty or Invalid Field';
 
-    if (response.ok) {
-      myModal.hide(); 
-      location.reload(); 
-    } else {
-      console.error('Error creating task:', response.status);
+            // Display the error message
+            const errorMessageElement = document.getElementById('error-message-task');
+            errorMessageElement.textContent = errorMessage;
+            errorMessageElement.style.display = 'block';
+            console.error('Error creating task:', response.status);
+        }
+    } catch (error) {
+        console.error('Error creating task:', error);
     }
-  } catch (error) {
-    console.error('Error creating task:', error);
-  }
 });
+
+function deleteTask(taskId) {
+    if (confirm('Are you sure you want to delete this task?')) {
+      fetch(`/tasks/${taskId}`, { method: 'DELETE' })
+        .then(response => {
+          if (response.ok) {
+            location.reload(); // Reload the page to update the task list
+          } else {
+            console.error('Error deleting task:', response.status);
+          }
+        })
+        .catch(error => {
+          console.error('Error:', error);
+        });
+    }
+  }
