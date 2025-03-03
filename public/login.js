3a283e83f68994e5237e5715599714fdb43f6be4
Maria Gabriela Martínez <gabrielamg@Marias-MacBook-Air.local>
2025-03-03 17:15:51 -0300

    user gets redirected to dashabord from home if its already logged in

--- public/login.js
+++ public/login.js
@@ -14,7 +14,23 @@ loginForm.addEventListener('submit', async (event) => {
         if (response.ok) {
             // Redirect to dashboard immediately after successful login
             window.location.href = '/dashboard';
-        } else {
+        } else if (response.status === 401) { // Check for 401 Unauthorized (token expired)
+            try {
+                const refreshResponse = await fetch('/refresh_token', {
+                    method: 'POST', 
+                });
+
+                if (refreshResponse.ok) {
+                    window.location.href = '/dashboard'; // Redirect
+                } else {
+                    // Refresh token is invalid or expired
+                    window.location.href = '/login'; // Redirect to login
+                    console.error("Refresh token error:", refreshResponse.status);
+                }
+            } catch (refreshError) {
+                console.error("No refresh token found:", refreshError);
+                window.location.href = '/login'; // Redirect to login
+            }
             // Handle login errors
             const errorData = await response.json();
             const errorMessage = errorData.message || 'Login failed.';
@@ -23,7 +39,7 @@ loginForm.addEventListener('submit', async (event) => {
             const errorMessageElement = document.getElementById('error-message');
             errorMessageElement.textContent = errorMessage;
             errorMessageElement.style.display = 'block';
-            console.error('Login failed:', response.status); 
+            console.error('Login failed:', response.status);
         }
     } catch (error) {
         console.error('Login error:', error); // Handle errors during the fetch request itself, such as network errors, connection issues,etc.
