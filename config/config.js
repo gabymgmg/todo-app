030d3eec7d896c35e997e486fb2c639092022c3a
Maria Gabriela Martínez <gabrielamg@Marias-MacBook-Air.local>
2025-03-05 19:21:21 -0300

    test object added to config

--- config/config.js
+++ config/config.js
@@ -30,6 +30,20 @@ module.exports = {
       secure: true, 
       maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days in milliseconds
     }
-  }
+  },
+  test: {
+    username: process.env.TEST_DBUSER,
+    password: process.env.TEST_DBPASSWORD,
+    database: process.env.TEST_PGDATABASE,
+    host: process.env.TEST_PGHOST,
+    port: process.env.TEST_PGPORT,
+    dialect: "postgres",
+    logging: false,
+    cookie: {
+      httpOnly: true,
+      secure: false, // Set to false during test.
+      maxAge: 7 * 24 * 60 * 60 * 1000
+    }
+  },
 }
 
