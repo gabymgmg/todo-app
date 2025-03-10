81e949103aa8ae33c966168f4194cc3015d98a18
Maria Gabriela Martínez <gabrielamg@Marias-MacBook-Air.local>
2025-03-10 15:34:53 -0300

    Filename changed. Some new messages for auth added

---
+++ utils/messages.js
@@ -0,0 +1,18 @@
+module.exports = {
+    REGISTRATION_SUCCESS: 'Registration Successful',
+    REGISTRATION_ERROR: 'Error in registering user',
+    USER_ALREADY_EXISTS: 'A user account already exists with this email',
+    LOGIN_SUCCESS: 'Login Successful',
+    INVALID_CREDENTIALS: 'Invalid Credentials',
+    AUTHENTICATION_ERROR: 'Authentication Error',
+    LOGOUT_SUCCESS: 'Logout Successful',
+    PLEASE_FILL_ALL_FIELDS: 'Please fill all fields',
+    INCORRECT_PASSWORD: 'Incorrect password',
+    USER_NOT_FOUND: 'User not found',
+    TASK_CREATED: 'Task created successfully',
+    TASK_CREATION_ERROR: 'Failed to create task',
+    TASK_RETURNED: 'Tasks returned successfully',
+    TASK_FAILED_RETURN: 'Failed getting tasks',
+        
+
+  };
